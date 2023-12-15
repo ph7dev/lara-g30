5fdc6f4aab571820945880d1f4f7564bc23eaa99
Vyacheslav <ph7dev@gmail.com>
2023-12-15 10:14:17 +0200

    added sorting to list table view

--- app/Livewire/Table.php
+++ app/Livewire/Table.php
@@ -8,10 +8,15 @@ use Livewire\WithPagination;
 
 abstract class Table extends Component
 {
-    use WithPagination; //spa pagination for Livewire pages
+    use WithPagination;
+
+    //spa pagination for Livewire pages
 
     public int $perPage = 10;
     public int $page = 1;
+
+    public $sortBy = '';
+    public $sortDirection = 'asc';
 
     public abstract function query(): Builder;
 
@@ -23,7 +28,26 @@ abstract class Table extends Component
 
     public function data()
     {
-        return $this->query()->paginate($this->perPage);
+        return $this
+            ->query()
+            ->when($this->sortBy !== '', function ($query) {
+                $query->orderBy($this->sortBy, $this->sortDirection);
+            })
+            ->paginate($this->perPage);
+    }
+
+    public function sort(string $key)
+    {
+        $this->resetPage(); //скидаемо налаштування пагінатора, щоб перемалювати сторінку
+
+        if ($this->sortBy === $key) {
+            $direction = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+            $this->sortDirection = $direction;
+            return;
+        }
+
+        $this->sortBy = $key;
+        $this->sortDirection = 'asc';
     }
 
     public function render()
